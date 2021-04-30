2bafb23ca19dcac7efbfc31c179758e4cdaa85aa
bardia <bardia.mojra@gmail.com>
2021-04-30 18:46:59 -0500

    renames files for codec reasons.

---
+++ labs/lab03/out/fname_lint.py
@@ -0,0 +1,27 @@
+
+import os
+
+srcDir = './'
+
+def main():
+
+  for i, file in enumerate(os.listdir(srcDir)):
+    #print(i, file)
+    newfile = file
+    newfile = newfile.replace('.out', '.txt')
+    src = srcDir+file
+    dest = srcDir+newfile
+    os.rename(src,dest)
+    #print(i, file)
+
+  print('files renamed... ')
+  for i, file in enumerate(os.listdir(srcDir)):
+    print(i, file)
+
+
+if __name__ == '__main__':
+
+  main()
+
+
+# EOF
